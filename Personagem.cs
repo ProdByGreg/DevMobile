86bbcb88474cef5a03c56748d909298c98c98d2c
Gregory Akauan <108442016+ProdByGreg@users.noreply.github.com>
2024-11-07 22:01:38 -0300

    Add files via upload

---
+++ Personagem.cs
@@ -0,0 +1,11 @@
+﻿namespace Personagem.Models
+{
+    public class Personagem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Sobrenome { get; set; } = string.Empty;
+        public string Fantasia { get; set; } = string.Empty;
+        public string Local { get; set; } = string.Empty;
+    }
+}
---
+++ Personagens.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Personagens.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Personagens : ControllerBase
+    {
+        private static List<Personagem> personagens = 
+            new List<Personagem>
+        {
+            new Personagem
+            {
+                Id = 1,
+                Name = "Greg",
+                Sobrenome = "Ak",
+                Fantasia = "Miranha",
+                Local = "Houston"
+            },
+            new Personagem
+            {
+                Id = 2,
+                Name = "Jhon",
+                Sobrenome = "Ak",
+                Fantasia = "Wolf",
+                Local = "NY"
+            },
+        };
+    }
+}
